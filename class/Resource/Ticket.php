4f2c8acdbdebec9f46355e54443861fd2db6f0fb
Stardog <mcnaney@gmail.com>
2016-01-07 15:29:04 -0500

    Fleshed out.

--- class/Resource/Ticket.php
+++ class/Resource/Ticket.php
@@ -8,30 +8,99 @@ namespace election\Resource;
  */
 class Ticket extends \Resource
 {
+    
+    /**
+     * The ticket is attached to this ballot
+     * @var \Variable\Integer
+     */
+    protected $ballot_id;
+    
     /**
      * @var \Variable\String
      */
     protected $title;
-    
+
     /**
      * @var \Variable\String
      */
     protected $platform;
-    
+
     /**
      * @var \Variable\String
      */
     protected $site_address;
     
-    protected $table = 'elect_ticket';
+    /**
+     * @var \Variable\Integer
+     */
+    protected $active;
     
+    /**
+     * @var string
+     */
+    protected $table = 'elect_ticket';
+
     public function __construct()
     {
         parent::__construct();
-        
+
+        $this->ballot_id = new \Variable\Integer(0, 'ballot_id');
         $this->title = new \Variable\String(null, 'title');
         $this->title->setLimit(200);
         $this->platform = new \Variable\String(null, 'platform');
+        $this->platform->allowNull(true);
         $this->site_address = new \Variable\Url(null, 'site_address');
+        $this->site_address->allowNull(true);
+        $this->active = new \Variable\Bool(true, 'active');
+    }
+
+    public function setBallotId($var)
+    {
+        $this->ballot_id->set($var);
+    }
+    
+    public function setTitle($var)
+    {
+        $this->title->set($var);
+    }
+
+    public function setPlatform($var)
+    {
+        $this->platform->set($var);
+    }
+
+    public function setSiteAddress($var)
+    {
+        $this->site_address->set($var);
+    }
+    
+    public function getBallotId()
+    {
+        return $this->ballot_id->get();
+    }
+
+    public function getTitle()
+    {
+        return $this->title->get();
+    }
+
+    public function getPlatform()
+    {
+        return $this->platform->get();
+    }
+
+    public function getSiteAddress()
+    {
+        return $this->site_address->get();
+    }
+
+    public function setActive($active)
+    {
+        $this->active->set($active);
+    }
+    
+    public function getActive()
+    {
+        return $this->active->get();
     }
 }
