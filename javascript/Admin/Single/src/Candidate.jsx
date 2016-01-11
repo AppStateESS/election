bbda421ea852833f0de7d3399fa126ab1dab383b
Stardog <mcnaney@gmail.com>
2016-01-11 07:52:42 -0500

    Start of Candidates interface

---
+++ javascript/Admin/Single/src/Candidate.jsx
@@ -0,0 +1,20 @@
+var Candidates = React.createClass({
+    getInitialState: function() {
+        return {
+        };
+    },
+
+    getDefaultProps: function() {
+        return {
+        };
+    },
+
+    render: function() {
+        return (
+            <div>
+                Candidates
+            </div>
+        );
+    }
+
+});
