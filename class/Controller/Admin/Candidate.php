3293e69003f82fcab4c9ed39588938d97996897a
Stardog <mcnaney@gmail.com>
2016-01-11 07:53:12 -0500

    Start of Candidates interface

---
+++ class/Controller/Admin/Candidate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace election\Controller\Admin;
+
+use election\Factory\Candidate as Factory;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+class Candidate extends \election\Controller\Base
+{
+    
+}
