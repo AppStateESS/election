716677dc8d2b4c95b5020764448f92ece55a1293
Stardog <mcnaney@gmail.com>
2016-01-25 16:53:48 -0500

    Removed getTabs method.

--- class/Controller/Base.php
+++ class/Controller/Base.php
@@ -8,7 +8,6 @@ namespace election\Controller;
  */
 abstract class Base extends \Http\Controller
 {
-
     public function get(\Request $request)
     {
         $data = array();
@@ -16,28 +15,4 @@ abstract class Base extends \Http\Controller
         $response = new \Response($view);
         return $response;
     }
-    
-    public function getTabs($active) {
-        $template = new \Template();
-        $template->setModuleTemplate('election', 'Admin/tabs.html');
-        $vars['single_active'] = null;
-        $vars['multiple_active'] = null;
-        $vars['referendum_active'] = null;
-        
-        switch ($active) {
-            case 'single':
-                $vars['single_active'] = 'active';
-                break;
-            case 'multiple':
-                $vars['multiple_active'] = 'active';
-                break;
-            case 'referendum':
-                $vars['referendum_active'] = 'active';
-                break;
-        }
-        $template->addVariables($vars);
-        
-        return $template->get();
-    }
-
 }
