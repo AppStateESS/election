399bc882154a0b1f9ccb35c3cc1a8ed838bc92d0
Stardog <mcnaney@gmail.com>
2016-01-26 13:12:54 -0500

    Extending Ballot now. Description variable added.

--- class/Resource/Referendum.php
+++ class/Resource/Referendum.php
@@ -6,32 +6,29 @@ namespace election\Resource;
  * @license http://opensource.org/licenses/lgpl-3.0.html
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-class Referendum extends Base
+class Referendum extends Ballot
 {
-    /**
-     * @var \Variable\String
-     */
-    protected $title;
-
     /**
      * @var \Variable\String
      */
     protected $description;
 
-    /**
-     * @var \Variable\Integer
-     */
-    protected $electionId;
-    
     protected $table = 'elect_referendum';
 
     public function __construct()
     {
         parent::__construct();
-        $this->title = new \Variable\String(null, 'title');
-        $this->title->setLimit(255);
         $this->description = new \Variable\String(null, 'description');
-        $this->electionId = new \Variable\Integer(0, 'electionId');
+    }
+    
+    public function setDescription($var)
+    {
+        $this->description->set($var);
+    }
+    
+    public function getDescription()
+    {
+        return $this->description->get();
     }
 
 }
