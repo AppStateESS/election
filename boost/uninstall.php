8c8ba9bbfb089b4ba312b728eb44de8a2ae5be52
Stardog <mcnaney@gmail.com>
2016-01-27 16:55:50 -0500

    Added missing uninstall tables.

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -9,10 +9,14 @@ function election_uninstall(&$content)
 {
     $db = Database::newDB();
 
-    $db->buildTable('elect_ballot')->drop(true);
     $db->buildTable('elect_candidate')->drop(true);
+    $db->buildTable('elect_election')->drop(true);
+    $db->buildTable('elect_multiple')->drop(true);
+    $db->buildTable('elect_multi_chair_vote')->drop(true);
     $db->buildTable('elect_referendum')->drop(true);
-    $db->buildTable('elect_ref_option')->drop(true);
+    $db->buildTable('elect_referendum_vote')->drop(true);
+    $db->buildTable('elect_single')->drop(true);
+    $db->buildTable('elect_single_chair_vote')->drop(true);
     $db->buildTable('elect_ticket')->drop(true);
     $db->buildTable('elect_voter')->drop(true);
     return true;
