2773d0002cb5e21e5f7b4ec2262789232bf7e6fb
Stardog <mcnaney@gmail.com>
2016-01-27 09:29:22 -0500

    Beginning by loading data to facilitate the voting.

--- javascript/User/build/Election.js
+++ javascript/User/build/Election.js
@@ -4,9 +4,12 @@ var Election = React.createClass({
     getInitialState: function () {
         return {
             election: {},
-            student: {},
+            single: [],
+            multiple: [],
+            referendum: [],
             stage: 'single',
-            votes: {}
+            votes: {},
+            student: {}
         };
     },
 
@@ -14,7 +17,24 @@ var Election = React.createClass({
         this.load();
     },
 
-    load: function () {},
+    load: function () {
+        $.getJSON('election/User/Election', {
+            command: 'list'
+        }).done(function (data) {
+            this.setState({
+                election: data.election,
+                single: data.single,
+                multiple: data.multiple,
+                referendum: data.referendum
+            });
+        }.bind(this));
+    },
+
+    setStage: function (stage) {
+        this.setState({
+            stage: stage
+        });
+    },
 
     render: function () {
         var content = null;
--- javascript/User/src/Election.jsx
+++ javascript/User/src/Election.jsx
@@ -2,9 +2,12 @@ var Election = React.createClass({
     getInitialState: function() {
         return {
             election : {},
-            student : {},
+            single : [],
+            multiple : [],
+            referendum : [],
             stage : 'single',
-            votes : {}
+            votes : {},
+            student : {}
         };
     },
 
@@ -13,8 +16,23 @@ var Election = React.createClass({
     },
 
     load: function() {
+        $.getJSON('election/User/Election', {
+        	command : 'list'
+        }).done(function(data){
+            this.setState({
+                election : data.election,
+                single : data.single,
+                multiple : data.multiple,
+                referendum : data.referendum
+            });
+        }.bind(this));
     },
 
+    setStage : function(stage) {
+        this.setState({
+            stage : stage
+        });
+    },
 
     render: function() {
         var content = null;
