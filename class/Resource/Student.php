c295e2ede2cbb14f7d5979d27a3db633cd697c1f
Jeremy Booker <jeremy.booker@gmail.com>
2016-01-29 12:27:09 -0500

    Student class doesn't extend anything, so remove call to parent::__construct() in constructor.

--- class/Resource/Student.php
+++ class/Resource/Student.php
@@ -5,16 +5,16 @@ namespace election\Resource;
 /**
  * @license http://opensource.org/licenses/lgpl-3.0.html
  * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @author Jeremy Booker
  */
 class Student
 {
     protected $bannerId;
     protected $firstName;
     protected $lastName;
-    
+
     public function __construct()
     {
-        parent::__construct();
         $this->bannerId = new \Variable\Integer(null, 'bannerId');
         $this->firstName = new \Variable\String(null, 'firstName');
         $this->lastName = new \Variable\String(null, 'lastName');
