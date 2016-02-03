dd7dae5a18e8d346b56322daac2d591b8d19d744
Stardog <mcnaney@gmail.com>
2016-02-03 15:24:14 -0500

    Referendum display finished.

--- javascript/User/build/Referendum.js
+++ javascript/User/build/Referendum.js
@@ -15,12 +15,50 @@ var Referendum = React.createClass({
     },
 
     render: function () {
-        console.log(this.props.referendum);
-        return React.createElement(
-            "div",
+        var title = React.createElement(
+            "h2",
             null,
-            "Referendum"
+            this.props.referendum.title
         );
+        var body = BreakIt(this.props.referendum.description);
+        var footer = React.createElement(
+            "div",
+            { className: "row" },
+            React.createElement(
+                "div",
+                { className: "col-sm-4" },
+                React.createElement(
+                    "button",
+                    { className: "btn btn-block btn-lg btn-success",
+                        onClick: this.props.updateVote.bind(null, true) },
+                    React.createElement("i", { className: "fa fa-check" }),
+                    " Yes"
+                )
+            ),
+            React.createElement(
+                "div",
+                { className: "col-sm-4" },
+                React.createElement(
+                    "button",
+                    { className: "btn btn-block btn-lg btn-danger",
+                        onClick: this.props.updateVote.bind(null, false) },
+                    React.createElement("i", { className: "fa fa-times" }),
+                    " No"
+                )
+            ),
+            React.createElement(
+                "div",
+                { className: "col-sm-4" },
+                React.createElement(
+                    "button",
+                    { className: "btn btn-block btn-lg btn-warning",
+                        onClick: this.props.updateVote.bind(null, null) },
+                    "Abstain"
+                )
+            )
+        );
+
+        return React.createElement(Panel, { heading: title, body: body, footer: footer });
     }
 
 });
--- javascript/User/src/Referendum.jsx
+++ javascript/User/src/Referendum.jsx
@@ -14,11 +14,33 @@ var Referendum = React.createClass({
     },
 
     render: function() {
-        console.log(this.props.referendum);
-        return (
-            <div>
-                Referendum
+        var title = <h2>{this.props.referendum.title}</h2>;
+        var body = BreakIt(this.props.referendum.description);
+        var footer = (
+            <div className="row">
+                <div className="col-sm-4">
+                    <button className="btn btn-block btn-lg btn-success"
+                        onClick={this.props.updateVote.bind(null, true)}>
+                        <i className="fa fa-check"></i> Yes
+                    </button>
+                </div>
+                <div className="col-sm-4">
+                    <button className="btn btn-block btn-lg btn-danger"
+                        onClick={this.props.updateVote.bind(null, false)}>
+                        <i className="fa fa-times"></i> No
+                    </button>
+                </div>
+                <div className="col-sm-4">
+                    <button className="btn btn-block btn-lg btn-warning"
+                        onClick={this.props.updateVote.bind(null, null)}>
+                        Abstain
+                    </button>
+                </div>
             </div>
+        );
+
+        return (
+            <Panel heading={title} body={body} footer={footer}/>
         );
     }
 
