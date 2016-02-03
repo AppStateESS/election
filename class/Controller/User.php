d51ac68e1afc9c451717a280e6a05cc45e99f32c
Jeremy Booker <jeremy.booker@gmail.com>
2016-02-03 14:38:45 -0500

    Add controler to show "no elections open" message on user login.

--- class/Controller/User.php
+++ class/Controller/User.php
@@ -25,22 +25,33 @@ class User extends \Http\Controller
     {
         $command = $request->shiftCommand();
 
+        // Check for an open Election
+        $election = \election\Factory\Election::getCurrent();
+
+        if($election === false){
+            $command = 'NoOpenElections';
+        }
+
+        // Check to see if this student has already voted
+        // TODO
+
+        // Default command name is 'Election'
         if (empty($command)) {
             $command = 'Election';
         }
 
         $className = 'election\Controller\User\\' . $command;
         if (!class_exists($className)) {
-            throw new \Exception('Unknown command');
+            throw new \Exception('Unknown command: ' . $className);
         }
         $commandObject = new $className($this->getModule());
         return $commandObject;
     }
-        
+
     public static function loadNavBar()
     {
         $auth = \Current_User::getAuthorization();
-        
+
         $vars['is_admin'] = \Current_User::allow('election');
         $vars['logout_uri'] = $auth->logout_link;
         $vars['username'] = \Current_User::getDisplayName();
---
+++ class/Controller/User/NoOpenElections.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace election\Controller\User;
+
+/**
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ * @author Jeremy Booker
+ */
+class NoOpenElections extends \election\Controller\Base
+{
+
+    public function getHtmlView($data, \Request $request)
+    {
+        \Layout::addStyle('election', 'User/style.css');
+
+        // TODO.. Fill this in with an actual template
+        $content = 'No elections open right now.';
+
+        $view = new \View\HtmlView($content);
+        return $view;
+    }
+}
