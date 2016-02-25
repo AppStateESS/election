caf3ea99ba7f72b99b7c9d32fb8518b0f3236812
Matthew McNaney <mcnaney@gmail.com>
2016-02-25 15:11:43 -0500

    Add: Greek listing

---
+++ greek.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Listing of "greek" organization and the category they are in.
+ */
+$greeks = array(
+    'Alpha Sigma Phi' => 'interfraternity',
+    'Alpha Tau Omega' => 'interfraternity',
+    'Delta Chi' => 'interfraternity',
+    'Delta Sigma Phi' => 'interfraternity',
+    'Delta Tau Delta' => 'interfraternity',
+    'Kappa Alpha Order' => 'interfraternity',
+    'Kappa Sigma' => 'interfraternity',
+    'Phi Gamma Delta' => 'interfraternity',
+    'Phi Kappa Tau' => 'interfraternity',
+    'Phi Sigma Kappa' => 'interfraternity',
+    'Sigma Nu' => 'interfraternity',
+    'Sigma Alpha Epsilon' => 'interfraternity',
+    'Tau Kappa Epsilon' => 'interfraternity',
+    'Theta Chi' => 'interfraternity',
+    'Alpha Delta Pi' => 'panhellenic',
+    'Alpha Gamma Delta' => 'panhellenic',
+    'Omicron Pi' => 'panhellenic',
+    'Alpha Phi' => 'panhellenic',
+    'Chi Omega' => 'panhellenic',
+    'Delta Zeta' => 'panhellenic',
+    'Kappa Delta' => 'panhellenic',
+    'Sigma Kappa' => 'panhellenic',
+    'Phi Mu' => 'panhellenic',
+    'Zeta Tau Alpha' => 'panhellenic',
+    'Alpha Kappa Alpha' => 'nphc',
+    'Alpha Phi Alpha' => 'nphc',
+    'Delta Sigma Theta' => 'nphc',
+    'Kappa Alpha Psi' => 'nphc',
+    'Phi Beta Sigma' => 'nphc',
+    'Zeta Phi Beta' => 'nphc',
+    'Chi Upsilon Sigma' => 'multicultural',
+);
+?>
