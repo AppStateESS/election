f599714953019ad11ffb73fc2e0e9717815072d9
Matthew McNaney <mcnaney@gmail.com>
2016-08-18 11:21:32 -0400

    Removed fake data and moved to new FakeStudentProvider class

--- class/Factory/TestStudentProvider.php
+++ class/Factory/TestStudentProvider.php
@@ -10,37 +10,6 @@ namespace election\Factory;
  */
 class TestStudentProvider extends BannerStudentProvider {
 
-    protected function getFakeResponse()
-    {
-        $resp = Array();
-
-        $resp['ID'] = '900123456';
-        $resp['userName'] = 'BJooker';
-        $resp['firstName'] = 'Beremy';
-        $resp['lastName'] = 'Jooker';
-
-        $resp['gradYear'] = '18';
-        $resp['major'] = '219A';
-        $resp['onCampus'] = false;
-        $resp['studentLevel'] = BannerStudentProvider::UNDERGRAD;
-        $resp['creditHoursEnrolled'] = 14;
-        $resp['classification'] = BannerStudentProvider::JUNIOR;
-        $resp['transfer'] = false;
-        $resp['studentType'] = 'C';
-        $resp['collegeCode'] = 'AS';
-        $resp['collegeDesc'] = 'College of Arts & Sciences';
-        return $resp;
-    }
-
-    private function getFakeErrorResponse()
-    {
-        $obj = new \stdClass();
-
-        $obj->banner_id = '900123456';
-
-        return $obj;
-    }
-    
     public function pullStudentId()
     {
         return TEST_STUDENT_ID;
